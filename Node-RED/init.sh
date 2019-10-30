42327e78000ea32099d8cf15c9db34e0a4c281d9
JATG <jatg81@gmail.com>
2019-10-30 15:29:31 -0500

    Update init.sh

--- Node-RED/init.sh
+++ Node-RED/init.sh
@@ -2,6 +2,12 @@
 echo "heyss"
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 if [ $? == 0 ]; then
-echo "Se instalo nodered"
+  sudo systemctl enable nodered.service
+  cd ~/.node-red/node_modules
+  npm install node-red-node-wemo
+  npm install node-red-contrib-i2c
+  npm install node-red-node-emoncms
+  npm install node-red-contrib-mi-devices
+  npm install node-red-contrib-pid
 fi
-# sudo systemctl enable nodered.service
+
