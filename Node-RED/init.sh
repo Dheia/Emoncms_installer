c1a6360ab9c2dd4bf6bd067871d9f1c0af407365
JATG <jatg81@gmail.com>
2019-10-30 14:48:16 -0500

    Update init.sh

--- Node-RED/init.sh
+++ Node-RED/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "heyss"
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
-echo $?
 if [ $? == 0 ]; then
 echo "Se instalo nodered"
 fi
