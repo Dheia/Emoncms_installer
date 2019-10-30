f2f012c2b1f10ecc24d395b6cb810bf118303abf
JATG <jatg81@gmail.com>
2019-10-30 12:55:18 -0500

    Update init.sh

--- Node-RED/init.sh
+++ Node-RED/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "heyss"
-bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
