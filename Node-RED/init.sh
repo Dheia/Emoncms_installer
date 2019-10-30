d8a249a19782ee635f269deb6658ccc85b2260b1
JATG <jatg81@gmail.com>
2019-10-30 15:36:45 -0500

    Update init.sh

--- Node-RED/init.sh
+++ Node-RED/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "heyss"
+
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 if [ $? == 0 ]; then
   sudo systemctl enable nodered.service
