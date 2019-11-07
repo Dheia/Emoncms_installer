3237acc4cd9c8850f7ad74d68bf16f5f91c3fbc6
JATG <jatg81@gmail.com>
2019-11-06 19:18:12 -0500

    Update init.sh

--- UPS_Pico/init.sh
+++ UPS_Pico/init.sh
@@ -1,6 +1,8 @@
-#sudo apt-get install libcurl4-openssl-dev
-#sudo pip-3.2 install pycurl
+sudo apt-get install libcurl4-openssl-dev
+sudo pip-3.2 install pycurl
 cd package
 sudo python setup.py install
 cd ../picofssd
 sudo python setup.py install
+sudo update-rc.d picofssd defaults
+sudo update-rc.d picofssd enable
