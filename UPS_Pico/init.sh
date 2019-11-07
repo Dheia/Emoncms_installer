3845d772572096d3dc0070887b11c6d534560cf4
JATG <jatg81@gmail.com>
2019-11-07 04:46:08 -0500

    Update init.sh

--- UPS_Pico/init.sh
+++ UPS_Pico/init.sh
@@ -4,5 +4,8 @@ cd package
 sudo python setup.py install
 cd ../picofssd
 sudo python setup.py install
+if [ -f /etc/init.d/picofssd ]; then
+  chmod 755 /etc/init.d/picofssd
+fi
 sudo update-rc.d picofssd defaults
 #sudo update-rc.d picofssd enable
