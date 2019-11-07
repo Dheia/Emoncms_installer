45b5bb6704985396517fea422489af984444ede9
JATG <jatg81@gmail.com>
2019-11-07 12:14:35 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,9 +15,22 @@ git clone https://github.com/jatg81/Emoncms-Scripts.git
 cd $openenergymonitor_dir/Emoncms-Scripts
 git checkout master
 
-sudo chmod +x init.sh && ./init.sh
+OPTION=$(whiptail --title "Test Menu Dialog" --menu "Choose your option" 15 60 4 \
+"1" "Grilled Spicy Sausage" \
+"2" "Grilled Halloumi Cheese" \
+"3" "Charcoaled Chicken Wings" \
+"4" "Fried Aubergine"  3>&1 1>&2 2>&3)
+ 
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $OPTION
+else
+    echo "You chose Cancel."
+fi
 
-./main.sh
+#sudo chmod +x init.sh && ./init.sh
 
-cd UPS_Pico
-sudo chmod +x init.sh && ./init.sh
+#/main.sh
+
+#cd UPS_Pico
+#sudo chmod +x init.sh && ./init.sh
