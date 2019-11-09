fc901ceaeac2e3f5625e41598424e1025f4aa3c8
JATG <jatg81@gmail.com>
2019-11-08 23:48:51 -0500

    Update init.sh

--- Backup/init.sh
+++ Backup/init.sh
@@ -1,3 +1,8 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
-echo $DIR
-#/opt/emoncms/modules/backup
+cd $emoncms/modules/backup
+[ -f "emoncmcs-export.sh" ] && rm emoncmcs-export.sh
+[ -f "emoncmcs-import.sh" ] && rm emoncmcs-import.sh
+
+cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-export.sh $emoncms/modules/backup
+cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-import.sh $emoncms/modules/backup
+
+#
