d7c7e8b4f0745e4aa9260c2375a373a869b1fc2f
JATG <jatg81@gmail.com>
2019-11-09 19:55:03 -0500

    Update init.sh

--- Backup/init.sh
+++ Backup/init.sh
@@ -1,8 +1,11 @@
+var="//192.168.0.11/home/03_OpenEnergyMonitor/01_EmonPi/01_Backup_diario  /media/Emoncms_backup_diario cifs username=jatg,password=$pass 0 0"
 cd $emoncms/modules/backup
 [ -f "emoncmcs-export.sh" ] && rm emoncmcs-export.sh
 [ -f "emoncmcs-import.sh" ] && rm emoncmcs-import.sh
-#//192.168.0.11/home/03_OpenEnergyMonitor/01_EmonPi/01_Backup_diario  /media/Emoncms_backup_diario cifs username=jatg,password=$pass 0 0
+
 cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-export.sh $emoncms/modules/backup
 cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-import.sh $emoncms/modules/backup
+
+[ $(grep -c $var /etc/fstab) -eq 0 ] &&  echo $var | sudo tee -a /etc/fstab
 
 #crontab
