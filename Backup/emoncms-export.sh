569425ae8ea782f09587c0b12e4a2cda8b1fa576
JATG <jatg81@gmail.com>
2019-11-10 15:09:01 -0500

    Update emoncms-export.sh

--- Backup/emoncms-export.sh
+++ Backup/emoncms-export.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-log="/home/pi/data/emoncms-export.log"
-exec 1>>$log
 date=$(date +"%Y-%m-%d")
 SECONDS=0
 
@@ -8,46 +6,53 @@ echo "========================= Emoncms export start ===========================
 date
 echo "This export script has been modified by jatg"
 echo ""
-if [ -f /home/pi/backup/config.cfg ]
+if [ -f /opt/emon/modules/backup/config.cfg ]
 then
-    source /home/pi/backup/config.cfg
+    source /opt/emon/modules/backup/config.cfg
+    log="$backup_location/emoncms-export.log"
+    exec 1>>$log
+    echo "Log file: $log"
     echo "-----------------------------------------------------------------------------------------"
     echo "File config.cfg: "
-    echo "Location of database:       $mysql_path"
     echo "Location of emonhub.conf:   $emonhub_config_path"
     echo "Location of Emoncms:        $emoncms_location"
     echo "Location of Node Red:       $nodered_path"
     echo "Backup destinations:        $backup_location/export & $backup_location2"
     echo "-----------------------------------------------------------------------------------------"
 else
-    echo "ERROR: Backup /home/pi/backup/config.cfg file does not exist"
+    echo "ERROR: Backup /opt/emon/modules/backup/config.cfg file does not exist"
     exit 1
 fi
 
 export_file="$backup_location/export"
-phpfina="$mysql_path/phpfina"
-phptimeseries="$mysql_path/phptimeseries"
+phpfina="$database_path/phpfina"
+phptimeseries="$database_path/phptimeseries"
 emonhub_conf="$emonhub_config_path/emonhub.conf"
 emoncms_set="$emoncms_location/settings.php"
 bkp_tar="$backup_location/export/emoncms-backup-$date.tar"
 
-rm $export_file/*
+#rm $export_file/*
 
 #Avoid change files in phpfina and phptimeseries during backup 
-echo "- Stopping mqtt_input service"
-sudo service mqtt_input stop
+echo "- Stopping emoncms_mqtt service"
+sudo service emoncms_mqtt stop
+
+if [ ! -d  $backup_location/export ]; then
+	mkdir $backup_location/export
+	sudo chown pi $backup_location/export -R
+fi
 
 #phpfina
 echo "- Adding folder phpfina to: emoncms-backup-$date.tar"
 echo " "
-tar -c -C $backup_location phpfina |\
+tar -c -C $database_path phpfina |\
 pv -fptb -s $(du -sb $phpfina | awk '{print $1}') 2> >( while read -N 1 c;  do if [[ $c =~ $'\r' ]]; then sed -i "$ s/.*/  $pv_bar/g" $log; pv_bar=''; else pv_bar+="$c";  fi  done ) > $bkp_tar
 sleep 1;
 exec 1>>$log
 
 #phptimeseries
 echo "- Adding folder phptimeseries to: emoncms-backup-$date.tar"
-tar -rf $bkp_tar -C $backup_location phptimeseries 2>&1
+tar -rf $bkp_tar -C $database_path phptimeseries 2>&1
 
 if [ $? -ne 0 ]; then
     echo "Error: failed to tar phptimeseries data"
@@ -56,13 +61,13 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "- Restarting mqtt_input service"
-sudo service mqtt_input start > /dev/null
+echo "- Restarting emoncms_mqtt service"
+sudo service emoncms_mqtt start > /dev/null
 
 # Get MYSQL authentication details from settings.php
-if [ -f /home/pi/backup/get_emoncms_mysql_auth.php ]; then
-    auth=$(echo $emoncms_location | php /home/pi/backup/get_emoncms_mysql_auth.php php)
-    IFS=":" read username password <<< "$auth"
+if [ -f $backup_script_location/get_emoncms_mysql_auth.php ]; then
+    auth=$(echo $emoncms_location | php $backup_script_location/get_emoncms_mysql_auth.php php)
+    IFS=":" read username password database <<< "$auth"
 else
     echo "Error: cannot read MYSQL authentication details from Emoncms settings.php"
     echo "$PWD"
@@ -110,11 +115,13 @@ if [ $? -ne 0 ]; then
     rm -r $export_file/*
     exit 1
 fi
-
+sleep 1
 # Compress backup
 echo "- Compressing archive to gzip: emoncms-backup-$date.tar.gz"
 echo " "
+#pv -fptb -s $(du -sb $phpfina | awk '{print $1}') 2> >( while read -N 1 c;  do if [[ $c =~ $'\r' ]]; then sed -i "$ s/.*/  $pv_bar/g" $log; pv_bar=''; else pv_bar+="$c";  fi  done ) > $bkp_tar
 pv -fptb -s $(du -sb $bkp_tar | awk '{print $1}') $bkp_tar 2> >( while read -N 1 c; do if [[ $c =~ $'\r' ]]; then sed -i "$ s/.*/  $pv_bar/g" $log; pv_bar=''; else pv_bar+="$c";  fi  done ) | gzip > $bkp_tar.gz
+#pv -fptb -s $(du -sb $bkp_tar | awk '{print $1}') $bkp_tar 2> >( while read -N 1 c; do if [[ $c =~ $'\r' ]]; then sed -i "$ s/.*/  $pv_bar/g" $log; pv_bar=''; else pv_bar+="$c";  fi  done ) | gzip > $bkp_tar.gz
 sleep 1
 exec 1>>$log
 
@@ -129,6 +136,7 @@ echo "- Deleting temporary folder"
 rm -r $backup_location/export/temp
 rm $bkp_tar
 sudo cp $backup_location/export/emoncms-backup-$date.tar.gz $backup_location2
+rm $backup_location/export/emoncms-backup-$date.tar.gz
 
 duration=$SECONDS
 echo "" 
@@ -137,8 +145,6 @@ echo "    $(date)"
 echo "    Export time: $(($duration / 60)) min $(($duration % 60)) sec"
 echo "    Backup saved: $backup_location2/emoncms-backup-$date.tar.gz" 
 echo "    Size: $(( $( stat -c '%s' $backup_location2/emoncms-backup-$date.tar.gz ) / 1024 / 1024 )) MB" 
-echo "    Export finished...refresh page to view download link"
-echo "=========================================================================================" 
-echo " "
-echo "=== Emoncms export complete! ==="
+echo "    Export finished...refresh page to view download link" 
+echo "============================ Emoncms export complete! ===================================" 
 # The last line is identified in the interface to stop ongoing AJAX calls, please ammend in interface if changed here
