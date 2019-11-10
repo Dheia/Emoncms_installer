9e2d0085fab5fcefe0f733f6c9932bce85ed9858
JATG <jatg81@gmail.com>
2019-11-10 01:19:56 -0500

    Update init.sh

--- Backup/init.sh
+++ Backup/init.sh
@@ -1,12 +1,19 @@
-cd $emoncms/modules/backup
+#backup_script_location="/opt/emoncms/modules/backup"
+#backup_location="/opt/openenergymonitor/data"
+if [ -f /opt/emoncms/modules/backup/config.cfg ]
+
+#Replace import,export script with mod version
+cd $backup_script_location
 [ -f "emoncmcs-export.sh" ] && rm emoncmcs-export.sh
 [ -f "emoncmcs-import.sh" ] && rm emoncmcs-import.sh
+cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-export.sh $backup_script_location
+cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-import.sh $backup_script_location
 
-cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-export.sh $emoncms/modules/backup
-cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-import.sh $emoncms/modules/backup
-
+# Set Fstab file to mount NAS folder Backup_EmonPi
 var="//192.168.0.11/Backup_EmonPi  /media/Emoncms_backup_diario cifs username=emonpi,password=pi 0 0"
 [ $(grep -c "$var" /etc/fstab) -eq 0 ] && echo -e "\n$var" | sudo tee -a /etc/fstab
 sudo mount -a
-(crontab -l ; echo "00 4 * * * $emoncms/modules/backup/emoncms-export.sh >> $emoncms/data/emoncms-export.log 2>&1" ) | crontab -
+
+#Cron settings export backup automatically at 4:00
+(crontab -l ; echo "00 4 * * * $backup_script_location/emoncms-export.sh >> $backup_location/emoncms-export.log 2>&1" ) | crontab -
 #df -h
