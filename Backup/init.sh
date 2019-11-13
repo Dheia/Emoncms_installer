1cd94fa368cd462bf655ef0bc7c87a350860addc
JATG <jatg81@gmail.com>
2019-11-12 21:28:03 -0500

    Update init.sh

--- Backup/init.sh
+++ Backup/init.sh
@@ -1,33 +1,25 @@
-#!/bin/bash
-config_file="/opt/emoncms/modules/backup/config.cfg"
-log="/var/log/emoncms/exportbackup.log"
+nas_mount="/media/Emoncms_backup_diario"
+nas_device="//192.168.0.11/Backup_EmonPi"
+nas_user="emonpi"
+nas_pass="pi"
 
-if [ -f $config_file ]
-then
-  source $config_file
-  #Used for import export scripts to show progress bar
-  sudo apt-get install pv
-  #Replace import,export script with mod version
-  cd $backup_script_location
-  [ -f "emoncmcs-export.sh" ] && rm emoncmcs-export.sh
-  [ -f "emoncmcs-import.sh" ] && rm emoncmcs-import.sh
-  cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-export.sh $backup_script_location
-  cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-import.sh $backup_script_location
+if [ -f $emoncms_dir/modules/backup/config.cfg ]
 
-  # Set Fstab file to mount NAS folder Backup_EmonPi
-  smb="$nas_device  $nas_mount cifs username=emonpi,password=pi 0 0"
-  [ $(grep -c "$smb" /etc/fstab) -eq 0 ] && echo -e "\n$smb" | sudo tee -a /etc/fstab
-  sudo mount -a
+#Replace import,export script with mod version
+cd $backup_script_location
+[ -f "emoncmcs-export.sh" ] && rm emoncmcs-export.sh
+[ -f "emoncmcs-import.sh" ] && rm emoncmcs-import.sh
+cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-export.sh $backup_script_location
+cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-import.sh $backup_script_location
 
-  #Cron settings export backup automatically at 4:00
-  
-  #Using service-runner to run export script
-  (crontab -l ; echo "00 4 * * * redis-cli RPUSH service-runner $backup_script_location/emoncms-export.sh>$log") | crontab -
-  #redis-cli RPUSH service-runner "$backup_script_location/emoncms-export.sh /tmp/emoncms-flag-import>$log"
-  #Using export script directly
-  #(crontab -l ; echo "00 4 * * * $backup_script_location/emoncms-export.sh >> $backup_location/$log 2>&1" ) | crontab -
-  #df -h
-else
-  echo "ERROR: Backup $config_file file does not exist"
-  exit 1
-fi
+# Set Fstab file to mount NAS folder Backup_EmonPi
+smb="$nas_device  $nas_mount cifs username=emonpi,password=pi 0 0"
+[ $(grep -c "$smb" /etc/fstab) -eq 0 ] && echo -e "\n$smb" | sudo tee -a /etc/fstab
+sudo mount -a
+
+#Cron settings export backup automatically at 4:00
+#job_bkp=00 4 * * * $backup_script_location/emoncms-export.sh >> $backup_location/emoncms-export.log 2>&1
+#Using service-runner
+job_bkp="00 4 * * * ${backup_script_location}/emoncms-export.sh>${backup_script_location}/emoncms-export.log 2>&1"
+crontab -l | grep -Fq "$job_bkp"  || (crontab -l ; echo "$job_bkp" ) | crontab -
+#df -h
