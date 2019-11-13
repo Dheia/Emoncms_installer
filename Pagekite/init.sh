c5e84b6974233bee76641f83ff7fc2a573408075
JATG <jatg81@gmail.com>
2019-11-12 23:38:26 -0500

    Update init.sh

--- Pagekite/init.sh
+++ Pagekite/init.sh
@@ -1,1 +1,9 @@
-
+sudo apt-get install dirmngr
+# Add our repository to /etc/apt/sources.list
+echo deb http://pagekite.net/pk/deb/ pagekite main | sudo tee -a /etc/apt/sources.list
+# Add the PageKite packaging key to your key-ring
+sudo apt-key adv --recv-keys --keyserver keys.gnupg.net AED248B1C7B2CAC3
+# Refresh your package sources by issuing
+sudo apt-get update
+# Install pagekite
+sudo apt-get install pagekite
