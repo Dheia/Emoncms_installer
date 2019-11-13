8ec4abbc0333bcfa8dfed2890778035d500f12aa
JATG <jatg81@gmail.com>
2019-11-12 23:19:51 -0500

    Update init.sh

--- Backup/init.sh
+++ Backup/init.sh
@@ -12,18 +12,21 @@ then
   #Used for import export scripts to show progress bar
   sudo apt-get install pv
   #Replace import,export script with mod version
+  echo "Copying to backup folder import & export modified scripts"
   cd $backup_script_location
   [ -f "emoncmcs-export.sh" ] && rm emoncmcs-export.sh
   [ -f "emoncmcs-import.sh" ] && rm emoncmcs-import.sh
   cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-export.sh $backup_script_location
   cp $openenergymonitor_dir/Emoncms-Scripts/Backup/emoncmcs-import.sh $backup_script_location
-
+  
+  echo "Setting Fstab file to mount NAS folder Backup_EmonPi"
   # Set Fstab file to mount NAS folder Backup_EmonPi
   smb="$nas_device  $nas_mount cifs username=emonpi,password=pi 0 0"
   [ $(grep -c "$smb" /etc/fstab) -eq 0 ] && echo -e "\n$smb" | sudo tee -a /etc/fstab
   sudo mount -a
 
   #Cron settings export backup automatically at 4:00
+  echo "Setting backup job to cron to export backup automatically"
   #Using service-runner
   #redis-cli RPUSH service-runner "$backup_script_location/emoncms-export.sh /tmp/emoncms-flag-import>$log"
   job_bkp="00 4 * * * ${backup_script_location}/emoncms-export.sh>${backup_script_location}/emoncms-export.log 2>&1"
