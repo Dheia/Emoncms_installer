b16626d953331f25111276707a6788b5f72b5edb
JATG <jatg81@gmail.com>
2019-12-13 22:19:40 -0500

    Update init.sh

--- SSH_rootaccess/init.sh
+++ SSH_rootaccess/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /etc/ssh
-sudo sed 's/^PermitRootLogin\s.*$/PermitRootLogin Yes/' sshd_config
+sudo sed -i 's/^PermitRootLogin\s.*$/PermitRootLogin Yes/' sshd_config
 sudo service ssh reload
