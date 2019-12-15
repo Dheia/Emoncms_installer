e7cfbd603e7187452755334aa041a5a9ab030b8b
JATG <jatg81@gmail.com>
2019-12-15 00:11:03 -0500

    Update init.sh

--- RFM69Pi/init.sh
+++ RFM69Pi/init.sh
@@ -1,21 +1,34 @@
-
-[ $(grep -c "dtoverlay=pi3-disable-bt$" /boot/config.txt) -eq 0 ] && echo -e "\ndtoverlay=pi3-disable-bt" | sudo tee -a /boot/config.txt
-if [ $(grep -c "enable_uart=" /boot/config.txt) -eq 0 ]
-then
-    echo -e "enable_uart=1" | sudo tee -a /boot/config.txt
-else
-    sudo sed -i "s/^enable_uart=.*/enable_uart=1/" /boot/config.txt
-fi
-sudo sed -i 's/console=serial0,*[^ ]*[ \|$\]*//g' /boot/cmdline.txt
-sudo systemctl disable hciuart
-sudo apt install minicom -y
-
-
-stty -F /dev/ttyAMA0 speed 38400 cs8 -cstopb -parenb raw
-echo -ne "6i" > /dev/ttyAMA0
-echo -ne "210g" > /dev/ttyAMA0
-echo -ne "4b" > /dev/ttyAMA0
-echo -ne "1c" > /dev/ttyAMA0  #collect mode 1c dont send acks
-echo -ne "1q" > /dev/ttyAMA0
-
-minicom -b 38400 -D /dev/ttyAMA0
+while true; do      
+  OPTION=$(whiptail --title "Install RFM69Pi Menu Dialog" --menu "Choose your option" 15 60 2 \
+    "1" "Raspberry configuration (Reboot required)" \
+    "2" "RFM69Pi configuration" \
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+      case $OPTION in
+        1)
+            [ $(grep -c "dtoverlay=pi3-disable-bt$" /boot/config.txt) -eq 0 ] && echo -e "\ndtoverlay=pi3-disable-bt" | sudo tee -a /boot/config.txt
+            if [ $(grep -c "enable_uart=" /boot/config.txt) -eq 0 ]
+            then
+                echo -e "enable_uart=1" | sudo tee -a /boot/config.txt
+            else
+                sudo sed -i "s/^enable_uart=.*/enable_uart=1/" /boot/config.txt
+            fi
+            sudo sed -i 's/console=serial0,*[^ ]*[ \|$\]*//g' /boot/cmdline.txt
+            sudo systemctl disable hciuart
+            sudo reboot
+        ;;
+        2)
+            sudo apt install minicom -y
+            stty -F /dev/ttyAMA0 speed 38400 cs8 -cstopb -parenb raw
+            echo -ne "6i" > /dev/ttyAMA0
+            echo -ne "210g" > /dev/ttyAMA0
+            echo -ne "4b" > /dev/ttyAMA0
+            echo -ne "1c" > /dev/ttyAMA0  #collect mode 1c dont send acks
+            echo -ne "1q" > /dev/ttyAMA0
+            minicom -b 38400 -D /dev/ttyAMA0
+        ;;
+      esac
+    else
+      exit
+    fi
+done
