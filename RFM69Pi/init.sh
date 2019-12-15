6835898c376bd0715ec4a4e992353ce361f14584
JATG <jatg81@gmail.com>
2019-12-15 01:31:03 -0500

    Update init.sh

--- RFM69Pi/init.sh
+++ RFM69Pi/init.sh
@@ -15,8 +15,8 @@ while true; do
             fi
             sudo sed -i 's/console=serial0,*[^ ]*[ \|$\]*//g' /boot/cmdline.txt
             sudo systemctl disable hciuart
-            read -p "Do you want to restart now: y/n" var
-            if [$var ="y"]
+            read -p "Do you want to restart now: y/n: " var
+            if [$var = y ]
             then
                 echo "Restarting ...."
             fi
