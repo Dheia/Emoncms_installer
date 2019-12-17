78f5f960e3070c938718dd1e69a0cad341cdb2a0
JATG <jatg81@gmail.com>
2019-12-17 13:19:26 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -34,7 +34,7 @@ while true; do
         ;;
         2)
             cd /opt
-            wget https://raw.githubusercontent.com/openenergymonitor/EmonScripts/stable/install/init.sh
+            sudo wget https://raw.githubusercontent.com/openenergymonitor/EmonScripts/stable/install/init.sh
             sudo chmod +x init.sh && ./init.sh
             sudo rm init.sh
             
