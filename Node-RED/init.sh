916f27e62bc72cefc3becdc92cadd5f5f9b2b41a
JATG <jatg81@gmail.com>
2019-12-18 11:37:19 -0500

    Update init.sh

--- Node-RED/init.sh
+++ Node-RED/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 if [ $? == 0 ]; then
-  cd ~/.node-red/node_modules
+  cd ~/.node-red
   npm install node-red-node-wemo
   npm install https://github.com/nielsnl68/node-red-contrib-i2c.git   #npm install node-red-contrib-i2c
   npm install node-red-node-emoncms
