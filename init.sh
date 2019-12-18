90fe6634e84dbe663cfcf26f658080ad4720a73a
JATG <jatg81@gmail.com>
2019-12-17 23:06:17 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ source ./config.ini
 
 clear
 echo "Updating system and installing git package ..."
-echo "eta: $openenergymonitor_mod"
+echo "eta2: $openenergymonitor_mod"
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get dist-upgrade -y && sudo apt-get clean
 sudo apt --fix-broken install
