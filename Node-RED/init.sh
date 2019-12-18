fdb7dd7a354e118584e23d87f24def818de41393
JATG <jatg81@gmail.com>
2019-12-18 10:17:49 -0500

    Update init.sh

--- Node-RED/init.sh
+++ Node-RED/init.sh
@@ -3,11 +3,11 @@ bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/mast
 if [ $? == 0 ]; then
   sudo systemctl enable nodered.service
   cd ~/.node-red/node_modules
-  npm install node-red-node-wemo
-  npm install https://github.com/nielsnl68/node-red-contrib-i2c.git   #npm install node-red-contrib-i2c
-  npm install node-red-node-emoncms
-  npm install node-red-contrib-mi-devices
-  npm install node-red-contrib-pid
+  sudo npm install node-red-node-wemo
+  sudo npm install https://github.com/nielsnl68/node-red-contrib-i2c.git   #npm install node-red-contrib-i2c
+  sudo npm install node-red-node-emoncms
+  sudo npm install node-red-contrib-mi-devices
+  sudo npm install node-red-contrib-pid
   sudo service nodered restart
   echo "Node Red installed successfully..."
 fi
