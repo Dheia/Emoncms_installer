5e3506722e51248c675d28faff2c69b841e61f05
JATG <jatg81@gmail.com>
2019-12-18 11:50:30 -0500

    Update init.sh

--- Node-RED/init.sh
+++ Node-RED/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo rm -r ~/.node-red
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 if [ $? == 0 ]; then
   cd ~/.node-red
