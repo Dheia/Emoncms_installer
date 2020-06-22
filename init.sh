61fe446fdbeb9f23a3e02c43ae46a8aba8939f71
JATG <jatg81@gmail.com>
2020-06-22 12:43:06 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,12 +3,7 @@ openenergymonitor_mod=/opt/openenergymonitor_mod
 user=$USER
 
 clear
-#echo "-------------------------------------------------"
-#echo "Updating system and installing git package       "
-#echo "-------------------------------------------------"
-#sudo apt-get update && sudo apt-get upgrade
-#sudo apt-get dist-upgrade -y && sudo apt-get clean
-#sudo apt --fix-broken install
+echo "Installing git ...."
 sudo apt-get install -y git
 
 echo "Creating openenergymonitor_mod folder in /opt/ ...."
@@ -26,52 +21,61 @@ rm init.sh
 
 while true; do      
   OPTION=$(whiptail --title "Install Menu Dialog" --menu "Choose your option" 15 60 9 \
-    "1" "Install HDD usb (reboot required)" \
-    "2" "Install RFM69Pi (reboot required)" \
-    "3" "Install Emoncms" \
-    "4" "Install UPSPico" \
-    "5" "Install NodeRed" \
-    "6" "Install PageKite" \
-    "7" "Install Backup Module mod" \
-    "8" "Disable led Rpi & RFM69Pi" \
-    "9" "Enable SSH root access"  3>&1 1>&2 2>&3)
+    "1" "Update system" \
+    "2" "Install HDD usb (reboot required)" \
+    "3" "Install RFM69Pi (reboot required)" \
+    "4" "Install Emoncms" \
+    "5" "Install UPSPico" \
+    "6" "Install NodeRed" \
+    "7" "Install PageKite" \
+    "8" "Install Backup Module mod" \
+    "9" "Disable led Rpi & RFM69Pi" \
+    "10" "Enable SSH root access"  3>&1 1>&2 2>&3)
     exitstatus=$?
     if [ $exitstatus = 0 ]; then
       case $OPTION in
         1)
+           echo "-------------------------------------------------"
+           echo "Updating system and installing git package       "
+           echo "-------------------------------------------------"
+           sudo apt-get update && sudo apt-get upgrade
+           sudo apt-get dist-upgrade -y && sudo apt-get clean
+           sudo apt --fix-broken install
+        ;;   
+        2)
             cd $openenergymonitor_mod/Emoncms-Scripts/USB_HDD
             chmod +x usb_hdd
             sudo ./usb_hdd -d /dev/sda
         ;;
-        2)
+        3)
             cd $openenergymonitor_mod/Emoncms-Scripts/RFM69Pi
             chmod +x init.sh && ./init.sh
         ;;
-        3)
+        4)
             cd $openenergymonitor_mod/Emoncms-Scripts/Emoncms
             chmod +x init.sh && ./init.sh
         ;;
-        4)  
+        5)  
             cd $openenergymonitor_mod/Emoncms-Scripts/UPS_Pico
             chmod +x init.sh && ./init.sh
         ;;
-        5)
+        6)
             cd $openenergymonitor_mod/Emoncms-Scripts/Node-RED
             chmod +x init.sh && ./init.sh
         ;;
-        6)
+        7)
             cd $openenergymonitor_mod/Emoncms-Scripts/Pagekite
             chmod +x init.sh && ./init.sh
         ;;
-        7)
+        8)
             cd $openenergymonitor_mod/Emoncms-Scripts/Backup
             chmod +x init.sh && ./init.sh
         ;;
-        8)
+        9)
             cd $openenergymonitor_mod/Emoncms-Scripts/RPi_Dark_mode
             chmod +x init.sh && ./init.sh
         ;;
-        9)
+        10)
             cd $openenergymonitor_mod/Emoncms-Scripts/SSH_rootaccess
             chmod +x init.sh && ./init.sh
         ;;
